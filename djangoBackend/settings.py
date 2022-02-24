6e0ae7728da48580588c0c1675b8e54466fe1bae
Fabian te Raaij <486205@student.fontys.nl>
2022-02-24 15:35:18 +0100

    allow all hosts and created requirements.txt

--- djangoBackend/settings.py
+++ djangoBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7l-6@e8jk$1ja)@l4n%h95gd1&x7&+udkeg_r%jl=voe2%b=)^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
