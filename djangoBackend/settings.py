44373ad90bd6224ea09889e32cd8e89a9bf3f008
Mex Steens <470033@student.fontys.nl>
2022-03-10 15:39:32 +0100

    , vergeten bij cors

--- djangoBackend/settings.py
+++ djangoBackend/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://s8ifzokvp35u68fi.azurewebsites.net:443"
+    "https://s8ifzokvp35u68fi.azurewebsites.net:443",
     "http://localhost:8080",
     "http://127.0.0.1:8000",
 ]
