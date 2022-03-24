ead935895694abafc62e0fd4d89af4592f155d37
Fabian te Raaij <fabian.teraaij@student.fontys.nl>
2022-03-24 15:29:41 +0100

    Improved CORS settings

--- djangoBackend/settings.py
+++ djangoBackend/settings.py
@@ -53,7 +53,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'https://hij2ifw7oy23da2z.azurewebsites.net'
+]
 
 ROOT_URLCONF = 'djangoBackend.urls'
 
